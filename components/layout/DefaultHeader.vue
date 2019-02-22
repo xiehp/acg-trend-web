b728277826156c6e072bfac26968d5f955d37ab3
xie <xie@qq.com>
2019-02-22 19:41:13 +0800

    update

---
+++ components/layout/DefaultHeader.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
+      <v-list>
+        <v-list-tile v-for="(item, i) in items" :key="i" :to="item.to" router exact>
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar :clipped-left="clipped" fixed app>
+      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      </v-btn>
+      <v-btn icon @click.stop="clipped = !clipped">
+        <v-icon>web</v-icon>
+      </v-btn>
+      <v-btn icon @click.stop="fixed = !fixed">
+        <v-icon>remove</v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    data() {
+      return {
+        clipped: false,
+        drawer: false,
+        fixed: false,
+        headTitles: [
+          {
+            icon: 'apps',
+            title: 'Welcome',
+            to: '/'
+          },
+          {
+            icon: 'bubble_chart',
+            title: 'Inspire',
+            to: '/inspire'
+          },
+          {
+            icon: 'bubble_chart',
+            title: 'detail',
+            to: '/detail'
+          }
+        ],
+        items: [
+          {
+            icon: 'apps',
+            title: 'Welcome',
+            to: '/'
+          },
+          {
+            icon: 'bubble_chart',
+            title: 'Inspire',
+            to: '/inspire'
+          },
+          {
+            icon: 'bubble_chart',
+            title: 'detail',
+            to: '/detail'
+          }
+        ],
+        miniVariant: false,
+        right: true,
+        title: 'Vuetify.js'
+      }
+    }
+  }
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,68 +2,15 @@
   <div>
     <BackgroundStars></BackgroundStars>
     <v-app dark>
-      <v-navigation-drawer
-        v-model="drawer"
-        :mini-variant="miniVariant"
-        :clipped="clipped"
-        fixed
-        app
-      >
-        <v-list>
-          <v-list-tile
-            v-for="(item, i) in items"
-            :key="i"
-            :to="item.to"
-            router
-            exact
-          >
-            <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title v-text="item.title"/>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-      </v-navigation-drawer>
-
-      <v-toolbar
-        :clipped-left="clipped"
-        fixed
-        app
-      >
-        <v-toolbar-side-icon @click="drawer = !drawer"/>
-        <v-btn
-          icon
-          @click.stop="miniVariant = !miniVariant"
-        >
-          <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon>
-        </v-btn>
-        <v-btn
-          icon
-          @click.stop="clipped = !clipped"
-        >
-          <v-icon>web</v-icon>
-        </v-btn>
-        <v-btn
-          icon
-          @click.stop="fixed = !fixed"
-        >
-          <v-icon>remove</v-icon>
-        </v-btn>
-        <v-toolbar-title v-text="title"/>
-      </v-toolbar>
+      <DefaultHeader></DefaultHeader>
 
       <v-content>
         <v-container>
-          <nuxt/>
+          <nuxt></nuxt>
         </v-container>
       </v-content>
 
-      <v-footer
-        :fixed="fixed"
-        app
-      >
+      <v-footer :fixed="fixed" app>
         <span>&copy; 2019</span>
       </v-footer>
     </v-app>
@@ -72,16 +19,35 @@
 
 <script>
   import BackgroundStars from "../components/BackgroundStars/BackgroundStars";
+  import DefaultHeader from "../components/layout/DefaultHeader.vue";
 
   export default {
     components: {
       BackgroundStars,
+      DefaultHeader,
     },
     data() {
       return {
         clipped: false,
         drawer: false,
         fixed: false,
+        headTitles: [
+          {
+            icon: 'apps',
+            title: 'Welcome',
+            to: '/'
+          },
+          {
+            icon: 'bubble_chart',
+            title: 'Inspire',
+            to: '/inspire'
+          },
+          {
+            icon: 'bubble_chart',
+            title: 'detail',
+            to: '/detail'
+          }
+        ],
         items: [
           {
             icon: 'apps',
